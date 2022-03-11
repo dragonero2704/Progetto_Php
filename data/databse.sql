6bb6e252c142f1a25b475c111965bd90377bbe51
Roberto Rudi <roberto.rudi04@gmail.com>
2022-03-11 23:22:32 +0100

    added databse file

---
+++ data/databse.sql
@@ -0,0 +1,63 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versione server:              10.4.21-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win64
+-- HeidiSQL Versione:            11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dump della struttura del database php_gamestore
+DROP DATABASE IF EXISTS `php_gamestore`;
+CREATE DATABASE IF NOT EXISTS `php_gamestore` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `php_gamestore`;
+
+-- Dump della struttura di tabella php_gamestore.account
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE IF NOT EXISTS `account` (
+  `email` char(50) NOT NULL,
+  `password` char(100) NOT NULL,
+  `codice_utente` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `nickname` char(50) NOT NULL,
+  `nome` char(50) NOT NULL,
+  `cognome` char(50) NOT NULL,
+  `telefono` char(50) DEFAULT NULL,
+  `email_recupero` char(50) DEFAULT NULL,
+  `data_nascita` date NOT NULL,
+  `nazionalita` char(10) NOT NULL,
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `codice_utente` (`codice_utente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- Dump dei dati della tabella php_gamestore.account: ~1 rows (circa)
+DELETE FROM `account`;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` (`email`, `password`, `codice_utente`, `nickname`, `nome`, `cognome`, `telefono`, `email_recupero`, `data_nascita`, `nazionalita`) VALUES
+	('roberto.rudi04@gmail.com', '$2y$10$Zq8feZiFPC8O88lt8WypjezO1uw9sH7GnbuWIBpceGJ885BO0IgM6', 0000000001, 'dragonero2704', 'Roberto', 'Rudi', '3926043632', '', '2004-09-27', 'Italia');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+
+-- Dump della struttura di tabella php_gamestore.giochi
+DROP TABLE IF EXISTS `giochi`;
+CREATE TABLE IF NOT EXISTS `giochi` (
+  `codice_gioco` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
+  `descrizione` tinytext NOT NULL,
+  `titolo` char(50) NOT NULL,
+  `prezzo` float NOT NULL DEFAULT 0,
+  PRIMARY KEY (`codice_gioco`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dump dei dati della tabella php_gamestore.giochi: ~0 rows (circa)
+DELETE FROM `giochi`;
+/*!40000 ALTER TABLE `giochi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `giochi` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
