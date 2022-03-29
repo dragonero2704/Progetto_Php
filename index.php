647ba78ed4aec31f63929af21feaa0d21aa3980c
dragonero2704 <roberto.rudi04@gmail.com>
2022-03-29 13:57:01 +0200

    fix

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
     require('./data/session.php');
     if(isset($email)){
-        header("location: ./pages/explore.php?theme=default");
-    }else{
         header("location: ./pages/home.php?theme=default");
+    }else{
+        header("location: ./pages/explore.php?theme=default");
     }
 ?>
