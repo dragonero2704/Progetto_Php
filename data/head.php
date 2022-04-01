af616e40cd357dbe95545f32483bfbc225328deb
dragonero2704 <roberto.rudi04@gmail.com>
2022-04-01 11:00:56 +0200

    sium

--- data/head.php
+++ data/head.php
@@ -2,7 +2,7 @@
     echo '<!-- normalize css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous">';
     // custom css
-    if (basename($_SERVER["PHP_SELF"]) == 'index.php'){
+    if (basename($_SERVER["PHP_SELF"]) === 'index.php'){
         echo'<!-- my css -->
             <link rel="stylesheet" href="./style/style.css">
             ';
